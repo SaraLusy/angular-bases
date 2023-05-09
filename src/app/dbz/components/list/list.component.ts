7018a74f96955680eb1c60ce15fe076b989380d2
SaraPR5 <sperez@vailos.com>
2023-05-09 15:47:18 +0200

    Primer commit

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+
 
 @Component({
   selector: 'dbz-list',
@@ -12,6 +13,14 @@ export class ListComponent {
   public characterList: Character[] = [{
     name: 'Trunks',
     power: 10
-  }]
+  }];
+
+  @Output()
+  public onDelete: EventEmitter <string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if(!id) return;
+    this.onDelete.emit(id);
+  }
 
 }
